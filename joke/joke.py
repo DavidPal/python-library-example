f377e4353492acc6b6b77a5e98cb9703f8264e4a
David Pal <dapal@expedia.com>
2020-01-24 13:24:41 -0800

    Initial version

---
+++ joke/joke.py
@@ -0,0 +1,2 @@
+def tell_joke():
+    return "Why is 6 afraid of 7? Because 7 8 9."
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="Joke",
+    url="https://github.com/DavidPal/",
+    author="David Pal",
+    author_email="davidko.pal@gmail.com",
+    packages=["joke"],
+    version="0.1",
+    license="MIT",
+    description="An example of a python package",
+)
